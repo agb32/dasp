c22274547aef2f49574d1861a1124f1af76d118d
Ali Basden <a.g.basden@durham.ac.uk>
2013-04-08 13:01:15 +0100

    debug

--- util/analyse.py
+++ util/analyse.py
@@ -378,7 +378,7 @@ if __name__=="__main__":
     if len(sys.argv)>1:
         tmp=sys.argv[1]
         try:
-            port=int(tmp)
+            portlist=int(tmp)
             hostname="localhost"
         except:
             hostname=tmp
