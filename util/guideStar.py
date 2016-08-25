24e4bf101584ec9b9fa4218e3c66369792c5bf35
Alastair Basden <a.g.basden@durham.ac.uk>
2016-08-25 14:57:44 +0100

    NGS sky brightness changed to float by default (to match LGS)

--- util/guideStar.py
+++ util/guideStar.py
@@ -399,7 +399,7 @@ class NGS(util.atmos.source):
     fov - radius of the fov, not diam, in arcsec
     centroidPower: raise calibrated pixels to this power before cog/corr.
     """
-    def __init__(self,idstr,nsubx,theta,phi,phasesize,pupil,minarea=0.5,sig=None,sourcelam=None,phslam=None,reconList=None,nimg=None,nfft=None,clipsize=None,ncen=None,preBinningFactor=1,bglevel=0.,readoutNoise=0.,integSteps=1,rowint=None,threshType=0,latency=0,skyBrightness=0,floor=0.,seed=0,atmosPhaseType="phaseonly",addPoisson=1,spotpsf=None,opticalBinning=0,magicCentroiding=0,linearSteps=None,calNCoeff=0,stepRangeFrac=1,centWeight=None,correlationCentroiding=0,corrThresh=0,corrPattern=None,useBrightest=0,fov=0.,parabolicFit=0,gaussianFitVals=None,subapFlag=None,integStepFn=None,cameraImage=0,subapLocation=None,centroidPower=1.):
+    def __init__(self,idstr,nsubx,theta,phi,phasesize,pupil,minarea=0.5,sig=None,sourcelam=None,phslam=None,reconList=None,nimg=None,nfft=None,clipsize=None,ncen=None,preBinningFactor=1,bglevel=0.,readoutNoise=0.,integSteps=1,rowint=None,threshType=0,latency=0,skyBrightness=0.,floor=0.,seed=0,atmosPhaseType="phaseonly",addPoisson=1,spotpsf=None,opticalBinning=0,magicCentroiding=0,linearSteps=None,calNCoeff=0,stepRangeFrac=1,centWeight=None,correlationCentroiding=0,corrThresh=0,corrPattern=None,useBrightest=0,fov=0.,parabolicFit=0,gaussianFitVals=None,subapFlag=None,integStepFn=None,cameraImage=0,subapLocation=None,centroidPower=1.):
         """pupil can be a util.tel object"""
         #Initialise parent...
         super(NGS,self).__init__(idstr,theta,phi,-1,sourcelam=sourcelam,phslam=phslam,sig=sig)
